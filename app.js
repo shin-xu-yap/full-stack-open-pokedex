65ee2d58f2a61b7e912c1389c2a2cfca91e5bfca
shin-xu-yap <shinxu.yap@gmail.com>
2025-01-02 18:02:25 +0800

    add normal health check endpoint

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
 
-// app.get('/health', (req, res) => {
-//   res.send('ok')
-// })
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
+// for health check property
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw('error...  ')
